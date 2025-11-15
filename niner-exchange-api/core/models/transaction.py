b7d15bd6f5c5af9aeef5177ce82759afcb539a3f
PabloG55 <pasebarona@gmail.com>
2025-11-15 18:53:41 -0500

    send message button working

--- niner-exchange-api/core/models/transaction.py
+++ niner-exchange-api/core/models/transaction.py
@@ -6,21 +6,28 @@ import uuid
 from core.models.listing import Listing
 from core.models.meetup_location import MeetupLocation
 
+
 class Transaction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True, blank=True)
-    buyer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='purchases', on_delete=models.CASCADE)
-    seller = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sales', on_delete=models.CASCADE)
-    meetup_location = models.ForeignKey(MeetupLocation, on_delete=models.SET_NULL, null=True, blank=True)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="purchases", on_delete=models.CASCADE
+    )
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="sales", on_delete=models.CASCADE
+    )
+    meetup_location = models.ForeignKey(
+        MeetupLocation, on_delete=models.SET_NULL, null=True, blank=True
+    )
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     STATUS_CHOICES = [
-        ('PENDING', 'Pending'),
-        ('ACCEPTED', 'Accepted'),
-        ('REJECTED', 'Rejected'),
-        ('COMPLETED', 'Completed'),
+        ("PENDING", "Pending"),
+        ("ACCEPTED", "Accepted"),
+        ("REJECTED", "Rejected"),
+        ("COMPLETED", "Completed"),
     ]
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="PENDING")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

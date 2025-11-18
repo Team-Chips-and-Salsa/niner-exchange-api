b02cf076bc5761761e3ada4146e83cfe32e4c2ce
PabloG55 <pasebarona@gmail.com>
2025-11-17 23:59:03 -0500

    prod ready?

--- niner-exchange-api/niner_exchange_api/urls.py
+++ niner-exchange-api/niner_exchange_api/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include  # Add 'include' here
 
+from core.views.auth_views.health_view import HealthCheckView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/health/", HealthCheckView.as_view(), name="health-check"),
     path(
         "api/auth/", include("core.urls.auth_urls")
     ),  # Authentication endpoints (login, register)

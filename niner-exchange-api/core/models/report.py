66f544eb58d5d30be93aac1c0b9c9f81148a9d03
Nick Tingen <142860763+NickT26-Coder@users.noreply.github.com>
2025-11-21 11:42:09 -0500

    feat: View and approve or deny reported listings

---
+++ niner-exchange-api/core/models/report.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import UniqueConstraint
+
+# Used Gemini guided learning to help me figure out how to represent the three different types of reports by using a GFK
+class Report(models.Model):
+    reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # Content type I learned will help distinguish between different types of reports by assigning an ID to each model (user, review, listing)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # This refers to the listing's listing_id for example
+    object_id = models.UUIDField(primary_key=False)
+    # This holds the actual model of a listing for example. It is found by using content_type and object_id
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    REASON_CHOICES = [
+        ("SPAM", "Spam"),
+        ("INNAPROPRIATE", "Innapropriate"),
+        ("HARASSMENT", "Harassment"),
+        ("SCAM", "Scam"),
+        ("OTHER", "Other"),
+    ]
+
+    reason = models.CharField(max_length=13, choices=REASON_CHOICES)
+
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("APPROVED", "Approved"),
+        ("DENIED", "Denied"),
+    ]
+    status = models.CharField(max_length=13, choices=STATUS_CHOICES, default="PENDING")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['object_id', 'reporter'], name='only-one-report')
+        ]
---
+++ niner-exchange-api/core/serializers/report_serializer.py
@@ -0,0 +1,67 @@
+from rest_framework import serializers
+from core.models.report import Report
+from core.models.listing import Listing
+from core.models.user import CustomUser
+from core.serializers.listing_serializer import ListingSellerSerializer, ListingSerializer
+
+class ReportSerializer(serializers.ModelSerializer):
+    reporter = ListingSellerSerializer(read_only=True)
+    content_object = serializers.SerializerMethodField()
+
+    # Used to distinguish between teh different types of reports
+    def get_content_object(self, obj):
+        target_object = obj.content_object
+
+        if isinstance(target_object, Listing):
+            return ListingSerializer(target_object, context=self.context).data
+        elif isinstance(target_object, CustomUser):
+            return ListingSellerSerializer(target_object, context=self.context).data
+        # elif isinstance(target_object, Review):
+        #     return ReviewSerializer(target_object, context=self.context).data
+
+    class Meta:
+        model = Report
+        fields = [
+            "id",
+            'content_type',
+            'object_id',
+            'content_object',
+            'reporter',
+            'reason',
+            'status',
+            'created_at',
+        ]
+
+        read_only_fields = ["id", "reporter", "created_at", "content_object"]
+
+    def create(self, validated_data):
+        validated_data['reporter'] = self.context['request'].user
+        return super().create(validated_data)
+    
+    
+class ReportStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Report
+        fields = ["status"]
+
+    def validate_status(self, value):
+        instance = self.instance
+
+        valid_statuses = [choice[0] for choice in Report.STATUS_CHOICES]
+        if value not in valid_statuses:
+            raise serializers.ValidationError(f"'{value}' is not a valid status.")
+
+        current_status = instance.status
+
+        if current_status == "APPROVED":
+            raise serializers.ValidationError(
+                "A 'APPROVED' report's status cannot be changed."
+            )
+
+        if current_status == "DENIED":
+            raise serializers.ValidationError(
+                "A 'DENIED' report's status cannot be changed."
+            )
+
+        return value
